64800866a54d1f8ac59db23a2b6367d0fb3fbc85
Alkurbi <61949384+Alkurbi@users.noreply.github.com>
2022-03-10 20:35:09 +0300

    Updates

---
+++ precache-manifest.15865df243effc0774efbdabb9805dc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6772903c8b6b2d7ac0c",
+    "url": "/react-portfolio/static/css/main.fb847121.chunk.css"
+  },
+  {
+    "revision": "f6772903c8b6b2d7ac0c",
+    "url": "/react-portfolio/static/js/main.f6772903.chunk.js"
+  },
+  {
+    "revision": "75aa2d861595ad62f4c5",
+    "url": "/react-portfolio/static/js/1.75aa2d86.chunk.js"
+  },
+  {
+    "revision": "ce722cbe40a4643d430e",
+    "url": "/react-portfolio/static/js/2.ce722cbe.chunk.js"
+  },
+  {
+    "revision": "fae8825402c70286ed73",
+    "url": "/react-portfolio/static/js/runtime~main.fae88254.js"
+  },
+  {
+    "revision": "da961f7bb5154e21b9bb7c87b6458f68",
+    "url": "/react-portfolio/static/media/background.da961f7b.jpg"
+  },
+  {
+    "revision": "b908ca872158c3ad5c51cf16b4f47dca",
+    "url": "/react-portfolio/static/media/welcome.b908ca87.jpg"
+  },
+  {
+    "revision": "a2bcd7f31b89d9dcc6f1326b2cc765d9",
+    "url": "/react-portfolio/index.html"
+  }
+];
